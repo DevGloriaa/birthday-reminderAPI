e866dcd8a8f8ad2a23943dfc0ee87ce8af8ba71a
Gloria-Obiorah <obiorahgloria911@gmail.com>
2025-12-27 17:12:29 +0100

    feat: birthday repo to find user by email

--- src/main/java/com/example/birthdayreminder/repository/BirthdayRepository.java
+++ src/main/java/com/example/birthdayreminder/repository/BirthdayRepository.java
@@ -1,4 +1,11 @@
 package com.example.birthdayreminder.repository;
 
-public interface BirthdayRepository {
+import com.example.birthdayreminder.model.Birthday;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface BirthdayRepository extends MongoRepository<Birthday, String> {
+
+    List<Birthday> findByUserEmail(String userEmail);
 }
