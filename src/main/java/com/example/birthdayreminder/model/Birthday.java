1c7d98bed0b094c935a18469d0e730f543446b86
Gloria-Obiorah <obiorahgloria911@gmail.com>
2025-12-27 17:11:40 +0100

    feat: birthday model with variables

--- src/main/java/com/example/birthdayreminder/model/Birthday.java
+++ src/main/java/com/example/birthdayreminder/model/Birthday.java
@@ -1,4 +1,23 @@
 package com.example.birthdayreminder.model;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Document(collection = "birthdays")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Birthday {
+
+    @Id
+    private String id;
+
+    private String name;
+    private LocalDate date;
+    private String note;
+
+    private String userEmail;
 }
